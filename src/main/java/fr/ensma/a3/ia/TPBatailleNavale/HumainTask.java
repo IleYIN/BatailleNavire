2d57b0e18c2b40a87e636c5a267d4c5d158dc147
yinyiliang <yiliang.yin@etu.isae-ensma.fr>
2019-01-11 13:36:54 +0100

    tp1

---
+++ src/main/java/fr/ensma/a3/ia/TPBatailleNavale/HumainTask.java
@@ -0,0 +1,20 @@
+package fr.ensma.a3.ia.TPBatailleNavale;
+
+/**
+ * Humain Task
+ * 
+ * @author yinyiliang
+ *
+ */
+public class HumainTask implements Runnable {
+
+	public HumainTask() {
+		Joueur humain = new Humain();
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/fr/ensma/a3/ia/TPBatailleNavale/OrdinateurTask.java
@@ -0,0 +1,19 @@
+package fr.ensma.a3.ia.TPBatailleNavale;
+
+/**
+ *  Ordinateur Task
+ * 
+ * @author yinyiliang
+ */
+public class OrdinateurTask implements Runnable {
+	
+	public OrdinateurTask() {
+		Joueur oridnateur = new Ordinateur();
+	}
+
+	public void run() {
+		// attendre la termination du joueur
+		
+	}
+
+}
